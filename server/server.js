fd985b4dc6ee3b2972bec25d74970eee3018c7db
Sheriffdeen <sherifdeenmuhammed@gmail.com>
2017-09-13 13:43:07 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+var {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -29,6 +29,29 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
   res.status(400).send(e);
 });
+});
+//GEt /TODOS/id
+app.get('/todos/:id',(req,res)=>{
+    var id =req.params.id;
+ if(!ObjectID.isValid(id)){
+   return res.status(404).send();
+ }
+ Todo.findById(id).then((todo)=>{
+   if(!todo){
+     return res.status(404).send();
+   }
+   res.send({todo});
+ }).catch((e)=>{
+   res.status(400).send();
+ });
+     //valid id using isValid
+     //404 send back an empty body
+     //find By id
+     //success
+     //if todo - send it back
+     //if no todo -send back 404 with empty body
+     //error
+     //400 - and send empty body back
 });
 
 app.listen(3000,()=>{
